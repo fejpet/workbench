3f66b1396510c1da1795c27f2c85336abfedf9f6
fejpet <fejp3t@gmail.com>
2022-12-16 05:23:30 -0800

    cert generator

--- CA/create.cmd
+++ CA/create.cmd
@@ -5,3 +5,6 @@ openssl req -new -x509 -days 3650 -key private\cakey.pem -out certs\cacert.crt -
 openssl genrsa -out private\userkey.pem 2048
 openssl req -new -key private\userkey.pem -reqexts v3_req -out certs\user.csr -nodes -subj "/CN=localhost"
 openssl x509 -req -days 3650 -in certs\user.csr -CA certs\cacert.crt -CAkey private\cakey.pem -extfile v3.cfg -out certs\user.crt
+openssl pkcs12 -export -in certs\user.crt -inkey private\userkey.pem -out certs\user.pfx -passout pass:
+powershell "Import-PfxCertificate -FilePath certs\user.pfx -CertStoreLocation Cert:\LocalMachine\Root"
+powershell "Import-Certificate -FilePath certs\cacert.crt -CertStoreLocation Cert:\LocalMachine\Root"
